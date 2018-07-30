25373e16ad2d73b2bf10be4b44cd6f4522e91419
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2018-07-30 13:43:39 +0300

    Edit typos, add cssMixins config

--- vcwb-element-boilerplate/component.js
+++ vcwb-element-boilerplate/component.js
@@ -6,11 +6,16 @@ export default class ElementBoilerplate extends vcvAPI.elementComponent {
   render () {
     let { id, atts, editor } = this.props
     let { output, customClass, metaCustomId } = atts // destructuring assignment for attributes from settings.json with access public
-    let textBlockClasses = 'vce-element-boilderplate-block'
-    let wrapperClasses = 'vce-element-boilderplate-wrapper vce'
+    let textBlockClasses = 'vce-element-boilerplate-block'
+    let wrapperClasses = 'vce-element-boilerplate-wrapper vce'
     let customProps = {}
     if (typeof customClass === 'string' && customClass) {
       textBlockClasses = textBlockClasses.concat(' ' + customClass)
+    }
+
+    let mixinData = this.getMixinData('boilerplateColorMixin')
+    if (mixinData) {
+      textBlockClasses.concat(` vce-element-boilerplate-block--color-${mixinData.selector}`)
     }
 
     if (metaCustomId) {
